41abae68e6e29a3e3dc8f772361fb703db1e0da5
Владислав Головатий <vladyslavgolovatyi@gmail.com>
2023-11-13 14:38:57 +0200

    commit

--- src/main/java/ua/lviv/iot/config/CustomSecurityConfig.java
+++ src/main/java/ua/lviv/iot/config/CustomSecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class CustomSecurityConfig {
 
     @Bean
